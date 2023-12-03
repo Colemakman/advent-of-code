7c4f9665c2a1c71d1fd2e35832e8283734342d89
Alfie <34726358+ToMuchTNT@users.noreply.github.com>
2023-12-03 08:42:25 +0000

    modified day3

--- 2023/Day3/day3a.py
+++ 2023/Day3/day3a.py
@@ -12,9 +12,7 @@ def parse_input(inp):
     return grid
 
 def check_valid(inp):
-
     sum = 0
-
     for line_index, line in enumerate(inp):
         for char_index, char in enumerate(line):
             if char.isdigit() and not line[char_index-1].isdigit():
@@ -22,14 +20,6 @@ def check_valid(inp):
                 while line[j].isdigit():
                     j += 1
                 else:
-                    print('------')
-                    print('NUM==', line[char_index:j])
-                    print('TOP=', inp[line_index-1][char_index-1:j+1])
-                    print('BELOW=', inp[line_index+1][char_index-1:j+1])
-                    print('LEFT=', line[char_index-1])
-                    print('RIGHT=', line[j])
-                    print(inp[line_index-1][char_index-1:j+1] == '.' * (j-char_index+2) and inp[line_index+1][char_index-1:j+1] == '.' * (j-char_index+2) and line[char_index-1] == '.' and line[j] == '.')
-                    print('--------')
                     if list(set(inp[line_index-1][char_index-1:j+1])) == ['.'] and list(set(inp[line_index+1][char_index-1:j+1])) == ['.'] and line[char_index-1] == '.' and line[j] == '.':
                         pass
                     else:
